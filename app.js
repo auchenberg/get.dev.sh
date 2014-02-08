f9f6d85556788c0cb535fdbfc5404b5dd9b3f1f4
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-02-07 22:56:55 -0800

    Initial work on get.dev.sh proxy.

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+
+var port = Number(process.env.PORT || 5000);
+
+app.get('/', function(req, res){
+	request.get('https://raw.github.com/auchenberg/dev/master/install.sh').pipe(res);
+});
+
+app.listen(port);
