d93967580de31357bfc53b27017f921a276e5751
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-02-09 08:25:06 -0800

    Updated repo url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var request = require('request');
 var port = Number(process.env.PORT || 5000);
 
 app.get('/', function(req, res){
-	request.get('https://raw.github.com/auchenberg/dev/master/install.sh').pipe(res);
+	request.get('https://raw.github.com/auchenberg/dotdev/master/install.sh').pipe(res);
 });
 
 app.listen(port);
